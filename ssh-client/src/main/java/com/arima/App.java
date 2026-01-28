b19f218ae43fe7265f4e7bc246142589bded4eb3
moadabdou <abdellawy553@gmail.com>
2026-01-28 11:19:05 +0100

    chore: init multi-module structure. Closes #1

---
+++ ssh-client/src/main/java/com/arima/App.java
@@ -0,0 +1,13 @@
+package com.arima;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
