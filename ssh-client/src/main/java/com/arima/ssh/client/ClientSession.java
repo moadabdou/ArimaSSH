a20eebd657372867ab5c5eb9ebaf584713c3b737
moadabdou <abdellawy553@gmail.com>
2026-02-16 22:54:42 +0000

    refactor:  removed unused imports

--- ssh-client/src/main/java/com/arima/ssh/client/ClientSession.java
+++ ssh-client/src/main/java/com/arima/ssh/client/ClientSession.java
@@ -7,7 +7,6 @@ import java.math.BigInteger;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.security.KeyPair;
-import java.security.Signature;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
